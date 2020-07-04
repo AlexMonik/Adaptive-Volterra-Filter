577613cff90a05dcafe316dba3a784d5c920eaf4
AlexMonik <64943432+AlexMonik@users.noreply.github.com>
2020-07-04 19:36:22 +0800

    update optimal filter
    minor refactoring

--- optimal_filter.py
+++ optimal_filter.py
@@ -1,54 +1,53 @@
-import pandas as pd
-import numpy as np
-import datetime as dt
-
-
-class OptimalFilter:
-    def __init__(self, typ):
-        """
-        :param typ: 1 - linear filter; 2 - nonlinear filter
-        """
+class Filter:
+    def __init__(self, filter_type):
         self.weights = None
-        self.typ = typ
+        self.filter_type = filter_type
+        if self.filter_type != 2 and self.filter_type != 1:
+            raise NameError('Type of filter should be 1 (linear) or 2 (nonlinear)')
 
     def fit(self, x, y):
         """
-        Calculate optimal weights of filter.
-        :param x: inputs (pandas.DataFrame or numpy.array with shape of M x N, M - number of realizations,
-                                                                               N - number of signal's samples)
-        :param y: outputs (M x 1)
-        :return: optimal weights (numpy.array; if typ=1 - linear weights, if typ=2 - linear and nonlinear)
+        calculate optimal weights
+        :param x: [list or numpy.nd array] input signal; 1st dimension - signal's realization (N)
+                                                         2nd dimension - signal's samples (M)
+        :param y: [list or numpy.nd array] output signal; shape must be (N,) or (N,1)
         """
-        if self.typ == 2:
-            x = OptimalFilter.trans(x)
-        elif self.typ == 1:
-            x = np.array(x)
-        else:
-            raise NameError('type of filter should be 1 (linear) or 2 (nonlinear) ')
-        y = np.array(y)
-        r = OptimalFilter.corr(x, x)
-        p = OptimalFilter.corr(x, y)
-        self.weights = np.linalg.inv(r) @ p
+        x = Filter.prepare_data(x)
+        y = Filter.prepare_data(y)
+        if x.shape[0] != y.shape[0]:
+            raise NameError('The number of input\'s and output\'s realizations is not the same')
+        if self.filter_type == 2:
+            x = Filter.trans(x)
+        r = Filter.corr(x, x)
+        p = Filter.corr(x, y)
+        self.weights = np.linalg.pinv(r) @ p
+        del r, p, x, y
 
     def predict(self, x):
-        """
-        Predict output by input vector.
-        :param x: input vector (pandas.DataFrame or numpy.array with shape of M x N, M - number of realizations,
-                                                                                     N - number of signal's samples)
-        :return: output vector
-        """
         if self.weights is None:
-            raise NameError('The weights of filter are undefined. Call method .fit(inputs, outputs) first.')
-        if self.typ == 2:
-            x = OptimalFilter.trans(x)
-        elif self.typ == 1:
-            x = np.array(x)
-        else:
-            raise NameError('type of filter should be 1 (linear) or 2 (nonlinear) ')
-        predicted = np.zeros([x.shape[0], 1])
+            raise NameError('The weights of the filter are undefined. Call method .fit(input, output) first.')
+        x = Filter.prepare_data(x)
+        if x.shape == (max(x.shape), 1):  # костыли
+            x = x.T
+        if self.filter_type == 2:
+            x = Filter.trans(x)
+        predicted = []
         for i in range(x.shape[0]):
-            predicted[i, 0] = self.weights.T @ x[i, :].reshape([x.shape[1], 1])
-        return predicted.reshape(x.shape[0])
+            rez = self.weights.T @ x[i, :]
+            predicted.append(rez[0])
+        return predicted
+
+    @staticmethod
+    def prepare_data(x):
+        if type(x) is not np.ndarray and type(x) is not list:
+            raise NameError('The type of signal must be either numpy.ndarray or list')
+        if type(x) is list:
+            x = np.array(x)
+        if len(x.shape) > 2:
+            raise NameError('The number of dimensions of signal can not be greater then 2')
+        if len(x.shape) == 1:
+            x = x.reshape((x.shape[0], 1))
+        return x
 
     @staticmethod
     def corr(x, y):
@@ -57,8 +56,8 @@ class OptimalFilter:
         """
         r = np.zeros([x.shape[0], x.shape[1], y.shape[1]])
         for i in range(x.shape[0]):
-            a = x[i, :].reshape([x.shape[1], 1])
-            b = y[i, :].reshape([1, y.shape[1]])
+            a = x[i, :].reshape((x.shape[1], 1))
+            b = y[i, :].reshape((1, y.shape[1]))
             r[i, :, :] = a @ b
         return r.mean(axis=0)
 
@@ -68,18 +67,36 @@ class OptimalFilter:
         Transform input signal vector into nonlinear form (second order)
         TODO add up-to p-th order
         :param x: linear form of input signal vector
-        :return: nonlinear form of input signal vector ([linear part, nonlinear part])
+        :return: nonlinear form of input signal vector
         """
-        x = np.array(x)
         n = 0
         for i in range(x.shape[1]):
-            n += (i+1)
-        ux = np.zeros([x.shape[0], x.shape[1]+n])
+            n += i + 1
+        ux = np.zeros((x.shape[0], x.shape[1] + n))
         for k in range(x.shape[0]):
             j1 = x.shape[1]
             for i in range(x.shape[1]):
                 ux[k, i] = x[k, i]
                 for j in range(i, x.shape[1]):
-                    ux[k, j1] = x[k, i]*x[k, j]
+                    ux[k, j1] = x[k, i] * x[k, j]
                     j1 += 1
         return ux
+    
+    @staticmethod
+    def make_s(a, M):
+        """
+        rearrange time-series to the array of filter's inputs
+        TODO now its work only with list; make it work with numpy.ndarray
+        :param a: origin_list
+        :param M: filter's tap
+        :return: new_x
+        """
+        x = []
+        y = []
+        for r in range(len(a) - M):
+            row = []
+            for c in range(M):
+                row.append(a[r + c])
+            x.append(row)
+            y.append(a[r + M])
+        return x, y
